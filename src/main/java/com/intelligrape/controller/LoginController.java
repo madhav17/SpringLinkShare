66af52e74eae356b251c90d3a75785d24fdab4c6
Madhav <madhav.khanna@tothenew.com>
2016-01-25 22:38:21 +0530

    Logout code is modified

--- src/main/java/com/intelligrape/controller/LoginController.java
+++ src/main/java/com/intelligrape/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 
@@ -79,8 +81,11 @@ public class LoginController {
     }
 
     @RequestMapping(value = "login/logout")
-    public String logout(HttpServletRequest request) {
-        request.removeAttribute("currentUser");
+    public String logout(HttpServletRequest request,HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
         return "redirect:/";
     }
 
